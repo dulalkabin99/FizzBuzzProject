883ecf98fdced77610c98e0977088d9201c64685
Kabin Dulal <dulalkabin99@gmail.com>
2018-06-08 09:39:52 -0400

    Shazam version of FizzBuzz

--- src/com/company/FizzBuzz.java
+++ src/com/company/FizzBuzz.java
@@ -1,35 +1,51 @@
 package com.company;
-
-
+import java.util.Random;
 public class FizzBuzz {
     static String pr;
+    Random rnd =new Random();
 
     public static void main(String[] args) {
         numPrint();
     }
 
-//creating method for the ligic
-    public static void numPrint() {
 
-        for (int i = 1; i <= 100; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0)) {
-                pr = "FizzBuzz";
-                print(pr);
-            } else if (i % 3 == 0) {
-                pr = "Fizz";
-                print(pr);
-            } else if (i % 5 == 0) {
-                pr = "Buzz";
-                print(pr);
-            } else {
-                pr = Integer.toString(i);
-                print(pr);
+    public static void numPrint() {
+        Random rnd =new Random();
+        int random1= rnd.nextInt(100)+1;
+        int random2 = rnd.nextInt(100)+1;
+
+
+        if (random1 == random2){
+            pr="Sazam";
+            print(pr);
+
+        }
+        else if (random1>random2) {
+            int temp=random1;
+            random1=random2;
+            random2=temp;
+            for (int i = random1; i <= random2; i++) {
+                if ((i % 3 == 0) && (i % 5 == 0)) {
+                    pr = "FizzBuzz";
+                    print(pr);
+                } else if (i % 3 == 0) {
+                    pr = "Fizz";
+                    print(pr);
+                } else if (i % 5 == 0) {
+                    pr = "Buzz";
+                    print(pr);
+                } else {
+                    pr = Integer.toString(i);
+                    print(pr);
+
+                }
             }
         }
     }
-// method to  print message
+
     public static void print(String pr) {
         System.out.println(pr);
 
     }
 }
+
